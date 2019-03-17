4bfe52818cb0a4c26d0d697a371cf12599e31497
hejunwei <846255299@qq.com>
2019-03-17 22:18:36 +0800

    新增歌曲列表

--- app/src/main/java/com/hjw/mediaplayerdemo/AudioActivity.java
+++ app/src/main/java/com/hjw/mediaplayerdemo/AudioActivity.java
@@ -1,9 +1,13 @@
 package com.hjw.mediaplayerdemo;
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -18,14 +22,21 @@ public class AudioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
         list = findViewById(R.id.list);
-        ContentResolver cp = getContentResolver();
-        Cursor cursor = cp.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+        final ContentResolver cp = getContentResolver();
+        final Cursor cursor = cp.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
                 null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
 
-        CursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.item_audio, cursor,
-                new String[]{MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ARTIST},
-                new int[]{R.id.txt_media_name, R.id.txt_audio_author},
-                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        CursorAdapter adapter = new AudioAdapter(this, cursor, true);
         list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cursor.moveToPosition(position);
+                Intent intent = new Intent();
+                intent.setData(Uri.parse(cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA))));
+                setResult(1, intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/hjw/mediaplayerdemo/MainActivity.java
+++ app/src/main/java/com/hjw/mediaplayerdemo/MainActivity.java
@@ -46,9 +46,8 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.btn_select_media: {
-//                    Intent intent = new Intent(MainActivity.this, AudioActivity.class);
-//                    startActivity(intent);
-                    updateMediaView();
+                    Intent intent = new Intent(MainActivity.this, AudioActivity.class);
+                    startActivityForResult(intent, 1);
                     break;
                 }
             }
@@ -58,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && data != null) {
+        if (requestCode == 1 && resultCode == 1 && data != null) {
             uri = data.getData();
             StringBuilder sb = new StringBuilder();
             sb.append("getAuthority = " + uri.getAuthority());
@@ -80,87 +79,6 @@ public class MainActivity extends AppCompatActivity {
 
             mMediaNameView.setText(sb.toString());
         }
-    }
-
-    private void updateMediaView() {
-        ContentResolver cp = getContentResolver();
-        Cursor cursor = cp.query(MediaStore.Audio.Media.INTERNAL_CONTENT_URI,
-                null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
-        String content = "";
-
-        content += "EXTERNAL_CONTENT_URI:\n" + Arrays.toString(cursor.getColumnNames()) + "\n";
-
-        cursor = cp.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-                null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
-
-        content += "\nINTERNAL_CONTENT_URI:\n" + Arrays.toString(cursor.getColumnNames());
-
-        content += "\n\n";
-        while (cursor.moveToNext()) {
-            //文件名
-            int displayNameColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME);
-            int contentTypeColumn = cursor.getColumnIndex(MediaStore.Audio.Media.CONTENT_TYPE);
-            int titleKeyColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
-            int entryContentTypeColumn = cursor.getColumnIndex(MediaStore.Audio.Media.ENTRY_CONTENT_TYPE);
-            int countColumn = cursor.getColumnIndex(MediaStore.Audio.Media._COUNT);
-            int idColumn = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
-            //歌曲名称
-            int titleColumn = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
-            //歌手
-            int artistColumn = cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-            //大小,单位byte
-            int sizeColumn = cursor.getColumnIndex(MediaStore.Audio.Media.SIZE);
-            //专辑
-            int albumColumn = cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM);
-            //时长，单位毫秒
-            int durationColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
-            int dataColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DATA);
-            //类型
-            int mimeTypeColumn = cursor.getColumnIndex(MediaStore.Audio.Media.MIME_TYPE);
-
-            if (displayNameColumn != -1) {
-                content += MediaStore.Audio.Media.DISPLAY_NAME + ":" + cursor.getString(displayNameColumn) + "\n";
-            }
-            if (contentTypeColumn != -1) {
-                content += MediaStore.Audio.Media.CONTENT_TYPE + ":" + cursor.getString(contentTypeColumn) + "\n";
-            }
-            if (titleKeyColumn != -1) {
-                content += MediaStore.Audio.Media.DEFAULT_SORT_ORDER + ":" + cursor.getString(titleKeyColumn) + "\n";
-            }
-            if (entryContentTypeColumn != -1) {
-                content += MediaStore.Audio.Media.ENTRY_CONTENT_TYPE + ":" + cursor.getString(entryContentTypeColumn) + "\n";
-            }
-            if (countColumn != -1) {
-                content += MediaStore.Audio.Media._COUNT + ":" + cursor.getInt(countColumn) + "\n";
-            }
-            if (idColumn != -1) {
-                content += MediaStore.Audio.Media._ID + ":" + cursor.getInt(idColumn) + "\n";
-            }
-            if (titleColumn != -1) {
-                content += MediaStore.Audio.Media.TITLE + ":" + cursor.getString(titleColumn) + "\n";
-            }
-            if (artistColumn != -1) {
-                content += MediaStore.Audio.Media.ARTIST + ":" + cursor.getString(artistColumn) + "\n";
-            }
-            if (sizeColumn != -1) {
-                content += MediaStore.Audio.Media.SIZE + ":" + cursor.getInt(sizeColumn) + "\n";
-            }
-            if (albumColumn != -1) {
-                content += MediaStore.Audio.Media.ALBUM + ":" + cursor.getString(albumColumn) + "\n";
-            }
-            if (durationColumn != -1) {
-                content += MediaStore.Audio.Media.DURATION + ":" + cursor.getLong(durationColumn) + "\n";
-            }
-            if (dataColumn != -1) {
-                content += MediaStore.Audio.Media.DATA + ":" + cursor.getString(dataColumn) + "\n";
-            }
-            if (mimeTypeColumn != -1) {
-                content += MediaStore.Audio.Media.MIME_TYPE + ":" + cursor.getString(mimeTypeColumn) + "\n";
-            }
-            content += "\n";
-        }
-
-        txt_all_info.setText(content);
     }
 
 }
